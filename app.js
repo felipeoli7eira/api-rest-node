e8b61068a3a02d08816980c413930b541e186bd8
felipeoli7eira <me.felipeoliveira@gmail.com>
2020-11-29 13:28:40 -0300

    endpoint /games, /game/:id e validacao da req

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /** require */
-const express = require("express")
+const express = require( "express" )
 const app = express()
 
 /** config */
@@ -10,20 +10,68 @@ const Database = {
 
     games: [
         {id: 1, name: 'Call of Dutty', year: 2009, price: 59.90},
-        {id: 1, name: 'GTA V', year: 2006, price: 30.20},
-        {id: 1, name: 'CS:GO', year: 2014, price: 29.50}
+        {id: 2, name: 'GTA V', year: 2006, price: 30.20},
+        {id: 3, name: 'CS:GO', year: 2014, price: 29.50}
     ]
 }
 
-function stts(code)
-{
-
-}
-
 /** route */
-app.get("/games", (req, res) => {
+app.get( "/games", (req, res) => {
 
-})
+    res.statusCode = 200
+    res.json( Database.games )
+} )
+
+app.get( "/game/:id", (req, res) => {
+
+    const {id} = req.params
+
+    if( Number.isNaN( id ) )
+    {
+        res.statusCode = 400 /** requisição inválida */
+
+        res.json(
+            {
+                message: "Requisição inválida (Bad Request)",
+                success: false,
+                statusCode: 400,
+                data: null
+            }
+        )
+        return false
+    }
+    else
+    {
+        let intID = parseInt( id )
+
+        let game = Database.games.find( game => game.id === intID )
+
+        if( game === undefined )
+        {
+            res.statusCode = 404 /** Recurso não encontrado (Not Found) */
+            res.json(
+                {
+                    message: "Recurso não encontrado (Not Found)",
+                    success: false,
+                    statusCode: 404,
+                    data: null
+                }
+            )
+            return false
+        }
+
+        res.statusCode = 200 /** Requisição bem sucedida */
+        res.json(
+            {
+                message: "Recurso encontrado",
+                success: true,
+                statusCode: 200,
+                data: game
+            }
+        )
+        return true
+    }
+} )
 
 /** listen */
-app.listen(8080, () => console.log("running..."))
+app.listen( 8080, () => console.log("running...") )
