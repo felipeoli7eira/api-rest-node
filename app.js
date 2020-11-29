96db96de7fddda5cc65b40bb37841b556533e410
felipeoli7eira <me.felipeoliveira@gmail.com>
2020-11-29 12:50:53 -0300

    configuracao do proj. e criacao de endpoints

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
+/** require */
 const express = require("express")
 const app = express()
 
+/** config */
 app.use( express.json() )
 app.use( express.urlencoded( {extended: false} ) )
+
+const Database = {
+
+    games: [
+        {id: 1, name: 'Call of Dutty', year: 2009, price: 59.90},
+        {id: 1, name: 'GTA V', year: 2006, price: 30.20},
+        {id: 1, name: 'CS:GO', year: 2014, price: 29.50}
+    ]
+}
+
+function stts(code)
+{
+
+}
+
+/** route */
+app.get("/games", (req, res) => {
+
+})
+
+/** listen */
+app.listen(8080, () => console.log("running..."))
